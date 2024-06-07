291c2ed1ce60326577e8749126f0ad8850ed176a
fathima-1234 <fathimasuhra19@gmail.com>
2024-06-07 22:32:56 +0530

    new commit

---
+++ title cast all words in string.py
@@ -0,0 +1,19 @@
+original_string = "hello world, how are you?"
+
+# Using title() method
+title_case_string = original_string.title()
+
+print(title_case_string)
+
+
+
+
+# def custom_title_case(input_string):
+#     return ' '.join(word.capitalize() for word in input_string.split())
+
+# original_string = "hello world, how are you?"
+
+# # Using custom_title_case function
+# title_case_string = custom_title_case(original_string)
+
+# print(title_case_string)
